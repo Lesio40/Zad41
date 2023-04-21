ddb461afb3d4a3470b373c83731fd4265766dfe2
Lesio40 <126687528+Lesio40@users.noreply.github.com>
2023-04-21 19:49:14 +0000

    Wszystko dziala

--- Student.java
+++ Student.java
@@ -20,7 +20,7 @@ public class Student {
 
   public static Student Parse(String str) {
     String[] data = str.split(" ");
-    if(data.length != 2) 
+    if(data.length != 3) 
       return new Student("Parse Error","Parse Error", -1);
     return new Student(data[0],data[1], Integer.parseInt(data[2]));
   }
